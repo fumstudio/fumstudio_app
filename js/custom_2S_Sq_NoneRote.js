3e951df6533a00c06809661e388fd3ccaa0019c7
fumstudio <ntuliqaphe@gmail.com>
2026-01-18 09:05:27 -0800

    Update WhatsApp number for sharing feature

--- js/custom_2S_Sq_NoneRote.js
+++ js/custom_2S_Sq_NoneRote.js
@@ -717,7 +717,7 @@ shareBtn.addEventListener('click', async () => {
     });
 
     // Prepare WhatsApp sharing
-    const whatsappNumber = "27728662309";
+    const whatsappNumber = "27626172613";
     const whatsappMessage = `Check out my design: ${shareableLink}`;
     const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
 
